3f26053ada8b2c0c2d9f1bbcdd3be23ecc21deb4
jomwasi <jomwasi@gmail.com>
2023-03-02 16:38:14 +0300

    connecting js & html

--- server.js
+++ server.js
@@ -6,4 +6,9 @@ const app = express();
 app.listen(3000, function() {
     console.log('listening on 3000')
   })
-  
+  app.get('/', (req, res) => {
+    res.send('Hello World')
+  })
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
