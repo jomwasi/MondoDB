1754bfc3e985ee30a0abad049905401261b00354
jomwasi <jomwasi@gmail.com>
2023-03-02 18:59:30 +0300

    vv

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.listen(3000, function() {
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-    app.post('/quotes', (req, res) =>{
-      console.log('Hellooooooooooooooooo!')
+  app.post('/quotes', (req, res) => {
+  console.log('Hellooooooooooooooooo!')
 })
